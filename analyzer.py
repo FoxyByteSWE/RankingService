3c2cb3c34cc23bd3636a24de70c70020f838e9f2
Luca Biasotto <luca.biasotto@gmail.com>
2022-06-28 20:17:56 +0200

    Ranking function based on list of posts

--- analyzer.py
+++ analyzer.py
@@ -3,7 +3,10 @@ from s3 import uploadFile, deleteFile
 import urllib.request
 import logging
 import boto3
+import pprint
 from botocore.exceptions import ClientError
+
+#from media import Media, json2Media
 
 BUCKET_NAME = "foxybyteswe"
 
@@ -58,27 +61,49 @@ def detectLabels(url):
 	response = parseImageResponse(response)
 	return response
 
-def ranking(scores):
-	pos = 0
-	neg = 0
-	neu = 0
-	mix = 0
-	for s in scores:
-		pos += s["Positive"]
-		neg += s["Negative"]
-		neu += s["Neutral"]
-		mix += s["Mixed"]
-	pos /= len(scores)
-	neg /= len(scores)
-	neu /= len(scores)
-	mix /= len(scores)
-	return pos - neg
+def rank(medias):
+
+	list = []
+	for m in medias:
+		if not list:
+			list.append([m])
+		else:
+			already_exists = False
+			for m_list in list:
+				if m.pk == m_list[0].pk:
+					m_list.append(m)
+					already_exists = True
+			if already_exists == False:
+				list.append([m])
+
+	dict = {}
+
+	for m_list in list:
+		pos = neg = neu = mix = 0
+		for m in m_list:
+			print(m.CaptionText)
+			score = analyzeText(m.CaptionText)
+			pos += score["Positive"]
+			neg += score["Negative"]
+			neu += score["Neutral"]
+			mix += score["Mixed"]
+		pos /= len(m_list)
+		neg /= len(m_list)
+		neu /= len(m_list)
+		mix /= len(m_list)
+		r = pos - neg
+		dict.update({ str(m_list[0].pk) : r })
+
+	return dict
 
 def main():
 	test = []
 	test.append(analyzeText("Molto bello!"))
 	test.append(analyzeText("Brutto"))
-	print(ranking(test))
+
+	#medias = json2Media((str(sys.path[0]))+"/data/locationsData.json")
+	#pprint.pprint(rank(medias))
+
 	detectLabels("https://instagram.ffco2-1.fna.fbcdn.net/v/t51.2885-15/11249882_966261376755731_963030927_n.jpg?se=8&stp=dst-jpg_e35&_nc_ht=instagram.ffco2-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=9lNYboVO5K0AX90TSMu&edm=AKmAybEBAAAA&ccb=7-5&ig_cache_key=MTIyOTEzNTQ0NTAwMDU1ODY0Mg%3D%3D.2-ccb7-5&oh=00_AT-H5SGET-X6zx_j-GGayPMijixUZwQOB6Ssy4c_gdtQSQ&oe=62BFFD3D&_nc_sid=bcb96")
 
 if __name__ == "__main__":
