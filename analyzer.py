1104b6456386dc1ae8fb7492fcbb94bb68b6df55
Luca Biasotto <luca.biasotto@gmail.com>
2022-07-01 15:25:18 +0200

    Parser for keyPhrases

--- analyzer.py
+++ analyzer.py
@@ -16,22 +16,6 @@ from restaurant import Restaurant, Media, json2Restaurants
 BUCKET_NAME = "foxybyteswe"
 
 ##### Comprehend
-def keyPhrases(text):
-	client = boto3.client('comprehend')
-
-	lang = client.detect_dominant_language(
-		Text = text
-	)
-	lang = lang["Languages"][0]["LanguageCode"]
-	#print(lang)
-
-	response = client.detect_key_phrases(
-		Text = text,
-		LanguageCode = lang
-	)
-	print(text)
-	print(response)
-
 def analyzeText(text):
 	client = boto3.client('comprehend')
 
@@ -60,6 +44,34 @@ def parseTextResponse(response):
 	dict["Mixed"] = response["SentimentScore"]["Mixed"]
 
 	return dict
+
+def keyPhrases(text):
+	client = boto3.client('comprehend')
+
+	lang = client.detect_dominant_language(
+		Text = text
+	)
+	lang = lang["Languages"][0]["LanguageCode"]
+	#print(lang)
+
+	response = client.detect_key_phrases(
+		Text = text,
+		LanguageCode = lang
+	)
+	response = parseKeyPhrase(response)
+	print(text)
+	print(response)
+
+def parseKeyPhrase(response):
+	list = []
+
+	for item in response["KeyPhrases"]:
+		dict = {}
+		dict["Accuracy"] = item["Score"]
+		dict["Text"] = item["Text"]
+		list.append(dict)
+
+	return list
 
 ##### Rekognition
 def detectLabels(url):
